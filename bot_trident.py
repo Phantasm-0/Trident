6ef686a984973a3c8d215cf7b0ab9007b509fcae
Phantasm-0 <68965404+Phantasm-0@users.noreply.github.com>
2020-09-23 21:06:51 +0300

    Update bot_trident.py

--- bot_trident.py
+++ bot_trident.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-import telebot,psycopg2, time, logging,htmlentities,re,urllib.parse,
+import telebot,psycopg2, time, logging,htmlentities,re,urllib.parse
 from psycopg2 import sql
 from telebot import types 
 import mobs.py as mobs
