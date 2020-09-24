538c02de1142f67ce4442b2d52a038b4f0e76dce
Vet Kos <gvcfhnkoo@gmail.com>
2020-09-24 16:28:27 +0300

    vet

--- bot_trident.py
+++ bot_trident.py
@@ -20,13 +20,12 @@ def update_helpers(call):
   number_result = number_result[0] + 1
   db.execute('SELECT helpers FROM MOBS  WHERE link = %s',(call.data,))
   result = db.fetchone()
-  result = str(result[0])  + "<b>" + str(number_result) + "." +"</b>" + call.from_user.first_name + "("+ "@" + call.from_user.username +  ")"
-  number_result = number_result + 1
+  result = str(result[0])  + "\n" + "<b>" + str(number_result) + "." +"</b>" + call.from_user.first_name + "("+ "@" + call.from_user.username +  ")"
   db.execute('UPDATE MOBS SET helpers = %s  WHERE link = %s',(result,call.data))
   conn.commit()
   db.execute('UPDATE MOBS SET helpers_number = %s  WHERE link = %s',(number_result,call.data))
+  conn.commit()
   RoyalTrident_bot.edit_message_reply_markup(call.message.chat.id,call.message.message_id,reply_markup = mobs_markups("⚔️ В бой","🤝 Помогаю",call.data) )
-
 
 
 
