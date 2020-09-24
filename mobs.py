e4d93be0697910bdc69696473e82fd0de2aa394a
Phantasm-0 <68965404+Phantasm-0@users.noreply.github.com>
2020-09-24 13:57:28 +0300

    Update mobs.py

--- mobs.py
+++ mobs.py
@@ -1,4 +1,4 @@
-import time,datetime,multiproccesing,psycopg2б,urllib.parse
+import time,datetime,psycopg2,urllib.parse
 from telebot import types
 
 RoyalTrident_bot = telebot.AsyncTeleBot('1222435814:AAFPEFv8ad_2xBIuYUMc5aIDxqKGhAKRijo')
