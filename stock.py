41b812fede72bcf63e089e1a9f12de15ac9dd8b3
Vet Kos <gvcfhnkoo@gmail.com>
2020-10-04 11:42:18 +0300

    nosql

--- stock.py
+++ stock.py
@@ -3,8 +3,6 @@ from telebot import types
 
 
 RoyalTrident_bot = telebot.AsyncTeleBot('1222435814:AAFPEFv8ad_2xBIuYUMc5aIDxqKGhAKRijo')
-conn = psycopg2.connect(database='postgres', user='postgres', password='123Anapa2017', host='localhost',port = 5432)
-db = conn.cursor()
 resource_pack = {'Thread': '01',
 'Stick': '02',
 'Pelt': '03',
